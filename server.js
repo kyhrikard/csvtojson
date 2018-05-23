3e725180c45eb54356c2b480f855adf8fd8c33dd
kyhrikard <rikard.carlsson@student.kyh.se>
2018-05-23 12:28:26 +0200

    Add cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const csv = require('csvtojson')
 const express = require('express')
 const request = require('request')
+const cors = require('cors')
 const port = process.env.PORT || 5000
 const app = express()
 
+app.use(cors())
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`)
 })
